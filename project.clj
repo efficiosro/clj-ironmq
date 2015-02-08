1236adf4f3447ca19528cc6129fe759e7965201a
Yury Yantsevich <featalion@gmail.com>
2015-02-08 18:18:48 +0300

    updates license and clojars info

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
 (defproject clj-ironmq "0.1.1-SNAPSHOT"
-  :description "Native Clojure IronMQ client library."
+  :description "A Clojure library for intercommunication with IronMQ v3 API."
   :url "https://github.com/efficiosro/clj-ironmq"
   :scm {:name "git", :url "https://github.com/efficiosro/clj-ironmq"}
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [cheshire "5.4.0"]
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+  :min-lein-version "2.0.0"
+  :dependencies [[cheshire "5.4.0"]
                  [clj-http "1.0.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}}
-  :aliases {"all" ["with-profile" "dev,1.3:dev,1.4:dev,1.5:dev,1.7:dev"]})
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha5"]]}}
+  :aliases {"all" ["with-profile" "dev,1.5:dev:dev,1.7"]}
+  :signing {:gpg-key "Yury Yantsevich (Efficio s.r.o.) <yury@efficio.cz>"})
